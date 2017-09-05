391e71f02d1a40b34ddc21e2d4ef84c8ec217ed8
farocean97 <farocean97@gmail.com>
2017-09-04 21:57:05 -0500

    new test project

---
+++ BinaryFileWriteRead/BinaryFileWriteRead/BinaryFileConvert.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualBasic.FileIO;
+using System.IO;
+
+namespace BinaryFileWriteRead
+{
+    public class BinaryFileConvert
+    {
+        public string strCSVFile { get; set; }
+        public string strBinaryFile { get; set; }
+
+
+        public BinaryFileConvert(string csvfile, string binaryfile)
+        {
+            strCSVFile = csvfile;
+            strBinaryFile = binaryfile;
+        }
+
+        public void ConvertCSVtoBinary()
+        {
+            if(File.Exists(strBinaryFile)) {
+                File.Delete(strBinaryFile);
+            }
+            using (BinaryWriter bw = new BinaryWriter(new FileStream(strBinaryFile,FileMode.Create))) {
+                using (TextFieldParser parser = new TextFieldParser(strCSVFile))
+                {
+                    parser.TextFieldType = FieldType.Delimited;
+                    parser.SetDelimiters(",");
+                    while (!parser.EndOfData)
+                    {
+                        string[] fields = parser.ReadFields();
+                        int intTime = Convert.ToInt32(fields[0]);
+                        double p1 = Convert.ToDouble(fields[1]);
+                        double p2 = Convert.ToDouble(fields[2]);
+
+                        bw.Write(intTime);
+                        bw.Write(p1);
+                        bw.Write(p2);
+                    }
+                }
+            
+            }
+
+        }
+
+        public void ConvertBinarytoCSV()
+        {
+            using (BinaryReader br = new BinaryReader(new FileStream(strBinaryFile, FileMode.Open)))
+            {
+                
+                using (StreamWriter sw = new StreamWriter(strCSVFile,false))
+                {
+                    while (br.BaseStream.Position != br.BaseStream.Length)
+                    {
+                        int inTime = br.ReadInt32();
+                        double p1 = br.ReadDouble();
+                        double p2 = br.ReadDouble();
+                        sw.WriteLine("{0},{1},{2}", inTime, p1, p2);
+                    }
+                }
+
+            }
+
+        }
+
+    }
+}
---
+++ BinaryFileWriteRead/BinaryFileWriteRead/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BinaryFileWriteRead
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strcsvfile = "D:\\Archive\\Pressure.csv";
+            string strbinfile = "D:\\Archive\\Pressure.dat";
+            string strcsv_converted = "D:\\Archive\\PressureConverted.csv";
+            bool BintoCsv = true;
+
+            if (!BintoCsv)
+            {
+                BinaryFileConvert bc = new BinaryFileConvert(strcsvfile, strbinfile);
+                bc.ConvertCSVtoBinary();
+            }
+            else
+            {
+                BinaryFileConvert bc2 = new BinaryFileConvert(strcsv_converted, strbinfile);
+                bc2.ConvertBinarytoCSV();
+            }
+
+
+
+            Console.WriteLine("Completed!");
+
+            Console.ReadLine();
+
+        }
+    }
+}
